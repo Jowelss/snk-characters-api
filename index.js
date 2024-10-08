1abee67ff71cc25981b1ae6bf1ae95d677ff1dee
Jowelss <devejowels.gitl.loper@gmail.com>
2024-10-08 17:45:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import characters, { data } from './character.json';
+import cors from 'cors';
+
+const app = express();
+
+app.user(cors());
+
+app.get('/characters', (req, res) => {
+  res.json(characters);
+});
+
+app.get('/characters/:id', (req, res) => {
+  const { id } = req.params;
+
+  const character = data.find((character) => character.id === id);
+
+  if (character) return res.json(character);
+
+  res.status(404).json({ message: 'Character not found' });
+});
+
+export default app;
